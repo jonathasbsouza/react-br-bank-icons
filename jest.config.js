ec04e48666036e18207dc7912f58e15f3b006d87
Caio Souza <caiobsouza@hotmail.com>
2020-05-23 18:07:35 -0300

    Setup jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts', '!src/serviceWorker.js'],
+  setupFiles: ['react-app-polyfill/jsdom', '<rootDir>/config/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/config/enzyme.setup.js'],
+  testMatch: ['<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
+  testEnvironment: 'jest-environment-jsdom-fourteen',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    // '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+    // '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js',
+  },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$', '^.+\\.module\\.(css|sass|scss)$'],
+  modulePaths: [],
+  moduleNameMapper: {
+    '^react-native$': 'react-native-web',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+  moduleFileExtensions: ['web.js', 'js', 'web.ts', 'ts', 'web.tsx', 'tsx', 'json', 'web.jsx', 'jsx', 'node'],
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+};
---
+++ src/specs/BankIcon.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import BankIcon from '../BankIcon';
+
+const defaultProps = {
+  bankName: 'itau',
+  bankId: 341,
+  size: 16,
+};
+
+const BankIconComponent = (props = defaultProps) => shallow(<BankIcon {...props} />);
+const findChild = (selector) => BankIconComponent().find(selector);
+
+describe('<BankIcon />', () => {
+  it('renders BankIcon component correctly', () => {
+    const component = renderer.create(<BankIcon {...defaultProps} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should do something', () => {
+    expect(true).toBeTruthy();
+  });
+});
