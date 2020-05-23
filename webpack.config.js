b774df5fe1c4fd46f3d006c04f112c3a79a53e63
Caio Souza <caiobsouza@hotmail.com>
2020-05-23 19:56:08 -0300

    Update webpack module rules

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
       {
         test: /\.js$/,
         include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|bower_components|build)/,
+        exclude: '/node_modules/',
         use: {
           loader: 'babel-loader',
           options: {
