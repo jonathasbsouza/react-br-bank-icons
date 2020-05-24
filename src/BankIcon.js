feeaf9d077a1fed60750f1b5f913aab9ebad5d0b
Jônathas Souza <jonathasbsouza@gmail.com>
2020-05-24 11:39:35 -0300

    add basic icon behavior

--- src/BankIcon.js
+++ src/BankIcon.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import { propTypes, defaultProps } from './types';
+import { getIconByName, getIconById } from './icon-set';
 
-const BankIcon = () => <div>Hello!</div>;
+const BankIcon = ({bankName, bankId, size, color}) => {
+
+    const bankIconData = getIconByName(bankName); 
+    const svgPath = (color ? bankIconData.svg.mono : bankIconData.svg.original );
+
+    return (
+        <svg width={size} className={'rbbi-' + bankIconData.bankName } height={size} viewBox="0 0 24 24" fill={ color } >
+            <title>{bankIconData.ariaTitle}</title>
+            <desc>{ 'Ícone representando a marca do ' + bankIconData.ariaTitle +'.' }</desc>
+            <g dangerouslySetInnerHTML={{__html: svgPath }} />
+        </svg>
+    )}
 
 BankIcon.propTypes = propTypes;
 BankIcon.defaultProps = defaultProps;
