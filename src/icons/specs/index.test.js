41e17a2ef0704b9e24a8674d0bc6425ff3d74079
Caio Souza <caiobsouza@hotmail.com>
2020-05-25 20:12:09 -0300

    Fix warning on icons unit test. Refactor icon unit test to have a dynamic describe

--- src/icons/specs/index.test.js
+++ src/icons/specs/index.test.js
@@ -4,40 +4,42 @@ import icons from '../';
 
 describe('Icons', () => {
   icons.map((icon) => {
-    it('should have a name', () => {
-      expect(icon.bankName).toBeTruthy();
-    });
+    describe(`Icon: ${icon.ariaTitle || icon.bankName}`, () => {
+      it('should have a name', () => {
+        expect(icon.bankName).toBeTruthy();
+      });
 
-    it('should have an id', () => {
-      expect(icon.bankId).toBeTruthy();
-    });
+      it('should have an id', () => {
+        expect(icon.bankId).toBeTruthy();
+      });
 
-    it('should have an ARIA title', () => {
-      expect(icon.ariaTitle).toBeTruthy();
-    });
+      it('should have an ARIA title', () => {
+        expect(icon.ariaTitle).toBeTruthy();
+      });
 
-    it('should have a svg JSON object', () => {
-      expect(typeof icon.svg).toBe('object');
-    });
+      it('should have a svg JSON object', () => {
+        expect(typeof icon.svg).toBe('object');
+      });
 
-    it('should have a <g>  inside svg.mono', () => {
-      const svg = shallow(icon.svg.mono);
-      expect(svg.html()).toContain('<g>')
-    });
+      it('should have a <g>  inside svg.mono', () => {
+        const svg = shallow(icon.svg.mono);
+        expect(svg.html()).toContain('<g>');
+      });
 
-    it('should have a <path> inside svg.mono', () => {
-      const svg = shallow(icon.svg.mono);
-      expect(svg.html()).toContain('<path d=')
-    });
+      it('should have a <path> inside svg.mono', () => {
+        const svg = shallow(icon.svg.mono);
+        expect(svg.html()).toContain('<path d=');
+      });
 
-    it('should have a <g> inside svg.original', () => {
-      const svg = shallow(icon.svg.original);
-      expect(svg.html()).toContain('<g>')
-    });
+      it('should have a <g> inside svg.original', () => {
+        const svg = shallow(<svg>{icon.svg.original}</svg>);
+        expect(svg.html()).toContain('<g>');
+      });
 
-    it('should have a <path> inside svg.original', () => {
-      const svg = shallow(icon.svg.original);
-      expect(svg.html()).toContain('<path d=')
+      it('should have a <path> inside svg.original', () => {
+        const svg = shallow(<svg>{icon.svg.original}</svg>);
+        expect(svg.html()).toContain('<path d=');
+      });
     });
   });
 });
