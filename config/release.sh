868a9f98304cc79c085f54b16b761630285016f1
Caio Souza <caiobsouza@hotmail.com>
2020-05-25 19:36:07 -0300

    finish setup release script. Add npm deploy config to travis.yml

--- config/release.sh
+++ config/release.sh
@@ -2,8 +2,9 @@ git fetch origin
 git checkout master
 git pull --rebase
 npm version patch -m "Bump up version to %s"
-git push origin master
+git push origin master --tags
 git checkout release
 git pull
 git merge -X theirs master
-git push origin release
+git push origin release --tags
+git checkout master
