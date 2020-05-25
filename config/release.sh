a75197beecc4214f10a772d0d897ba4cbeb4d7ec
Caio Souza <caiobsouza@hotmail.com>
2020-05-25 19:12:03 -0300

    (WIP) Implement release script

--- config/release.sh
+++ config/release.sh
@@ -1,1 +1,9 @@
+git fetch origin
+git checkout master
+git pull --rebase
 npm version patch -m "Bump up version to %s"
+git push origin master
+git checkout release
+git pull
+git merge -X theirs master
+git push origin release
