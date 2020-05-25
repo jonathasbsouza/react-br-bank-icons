c3998b684e037e3249d9c9dfce73e77e84c7ec2f
Italo <italusousa@gmail.com>
2020-05-25 19:31:34 -0300

    test cases for each way to select an icon

--- src/components/specs/BankIcon.test.js
+++ src/components/specs/BankIcon.test.js
@@ -5,31 +5,36 @@ import renderer from 'react-test-renderer';
 
 import BankIcon from '../BankIcon';
 
-const defaultProps = {
-  bankName: 'cajuina',
-  size: 16,
-};
+const defaultProps = { size: 16 };
 
 const BankIconComponent = (props = defaultProps) => shallow(<BankIcon {...props} />);
 const findChild = (selector) => BankIconComponent().find(selector);
 
 describe('<BankIcon />', () => {
-  it('renders BankIcon component correctly', () => {
-    const component = renderer.create(<BankIcon {...defaultProps} />);
+  it('renders BankIcon component correctly when BankName is provided', () => {
+    const component = renderer.create(<BankIcon {...defaultProps} bankName="cajuina" />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it('showld show console alert when both BankID and BankName are provided', () => {
+  it('renders BankIcon component correctly when BankId is provided', () => {
+    const component = renderer.create(<BankIcon {...defaultProps} bankId={-11} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('showld show console warn when both BankID and BankName are provided', () => {
     const originalWarn = console.warn;
-    const consoleSpy = jest.fn(); 
+    const consoleSpy = jest.fn();
     console.warn = consoleSpy;
-    renderer.create(<BankIcon {...defaultProps} bankId={-11} />);
+    renderer.create(<BankIcon {...defaultProps} bankName="cajuina" bankId={-11} />);
     expect(consoleSpy).toHaveBeenCalledWith('Both BankID and BankName were provided. BankName will be used.');
     console.warn = originalWarn;
-  });  
+  });
 
-  it('should do something', () => {
-    expect(true).toBeTruthy();
+  it('should render FallbackIcon when dont match any icon', () => {
+    const component = renderer.create(<BankIcon bankName="foo" size={16} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
