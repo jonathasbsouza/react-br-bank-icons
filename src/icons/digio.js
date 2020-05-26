4e0d32f6bfdf60552a4ed25c912b06703a9b2b01
Jônathas Souza <jonathasbsouza@gmail.com>
2020-05-26 00:37:05 -0300

    add all icons

--- src/icons/digio.js
+++ src/icons/digio.js
@@ -25,7 +25,3 @@ const Digio = {
 };
 
 export default Digio;
-
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fillRule="evenodd" clipRule="evenodd" fill="#3F3F40"/>
-</svg>
--- src/icons/index.js
+++ src/icons/index.js
@@ -7,11 +7,16 @@ import BS2 from './bs2';
 import BNB from './bnb'
 import BTG from './btg'
 import Caixa from './caixa'
+import Citibank from './citibank';
+import C6 from './c6';
+import Digio from './digio';
 import Inter from './inter'
 import Itau from './itau';
+import Neon from './neon';
 import Nubank from './nubank';
 import Original from './original';
-import Santander from './santander'
+import Safra from './safra';
+import Santander from './santander';
 import XP from './xp'
 
-export default [Agibank, Banrisul, Bradesco, BB, BMG, BS2, BNB, BTG, Caixa, Inter, Itau, Nubank, Original, Santander, XP ];
+export default [Agibank, Banrisul, BB, BMG, BNB, Bradesco, BS2, BTG, Caixa, Citibank, C6, Digio, Inter, Itau, Neon, Nubank, Original, Safra, Santander, XP ];
