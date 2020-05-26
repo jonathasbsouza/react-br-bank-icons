850eccc0b73eed4b25375d7c2bd8b192d1366d9a
Jônathas Souza <jonathasbsouza@gmail.com>
2020-05-25 23:25:34 -0300

    create BS2 and Nubank icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -3,13 +3,15 @@ import Banrisul from './banrisul'
 import BB from './bb';
 import BMG from './bmg';
 import Bradesco from './bradesco';
+import BS2 from './bs2';
 import BNB from './bnb'
 import BTG from './btg'
 import Caixa from './caixa'
 import Inter from './inter'
 import Itau from './itau';
+import Nubank from './nubank';
 import Original from './original';
 import Santander from './santander'
 import XP from './xp'
 
-export default [Agibank, Banrisul, Bradesco, BB, BMG, BNB, BTG, Caixa, Inter, Itau, Original, Santander, XP ];
+export default [Agibank, Banrisul, Bradesco, BB, BMG, BS2, BNB, BTG, Caixa, Inter, Itau, Nubank, Original, Santander, XP ];
