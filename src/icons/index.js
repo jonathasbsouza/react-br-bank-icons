bb28b2e7dd9c8df20023dd8b3c61abf7da0261e2
Jônathas Souza <jonathasbsouza@gmail.com>
2020-05-25 21:30:24 -0300

    Create Agibank, BTG & Original icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,10 +1,13 @@
+import Agibank from './agibank';
+import Banrisul from './banrisul'
 import BB from './bb';
 import BNB from './bnb'
-import Banrisul from './banrisul'
+import BTG from './btg'
 import Caixa from './caixa'
 import Inter from './inter'
 import Itau from './itau';
+import Original from './original';
 import Santander from './santander'
 import XP from './xp'
 
-export default [BB, BNB, Banrisul, Caixa, Inter, Itau, Santander, XP ];
+export default [Agibank, Banrisul, BB, BNB, BTG, Caixa, Inter, Itau, Original, Santander, XP ];
