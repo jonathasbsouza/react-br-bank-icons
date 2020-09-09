183ca76fe724783d7e006ec2e522c6d48b23f261
jesobreira <jefrey.santos@capitual.io>
2020-09-09 20:02:55 -0300

    cleaning agibank

--- src/icons/agibank.js
+++ src/icons/agibank.js
@@ -29,7 +29,3 @@ const Agibank = {
 };
 
 export default Agibank;
-
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fillRule="evenodd" clipRule="evenodd" fill="#3F3F40"/>
-</svg>
